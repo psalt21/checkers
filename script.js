d0fc9b3bb523cc847c2b3a3d048fab1f44230ace
Philip Salter <philipleesalter@gmail.com>
2016-03-10 20:44:47 -0700

    added pieces to boards and captured areas on each side

--- script.js
+++ script.js
@@ -1,4 +1,20 @@
-(function buildBoard(){
+var boardSquares = [
+  ['', 'red', '', 'red', '', 'red', '', 'red'],
+  ['red', '', 'red', '', 'red', '', 'red', ''],
+  ['', 'red', '', 'red', '', 'red', '', 'red'],
+  ['', '', '', '', '', '', '', ''],
+  ['', '', '', '', '', '', '', ''],
+  ['black', '', 'black', '', 'black', '', 'black', ''],
+  ['', 'black', '', 'black', '', 'black', '', 'black'],
+  ['black', '', 'black', '', 'black', '', 'black', ''],
+];
+
+(function setup(){
+  buildBoard();
+  setPieces();
+})();
+
+function buildBoard(){
   var colors = ['black', 'white', 'black', 'white', 'black', 'white', 'black', 'white', 'white', 'black', 'white', 'black', 'white', 'black', 'white', 'black',];
   var board = document.getElementById('checker-board');
   var rowId = 0;
@@ -9,7 +25,7 @@
     var row = document.createElement('div');
     row.className = 'row';
     row.id = 'row' + rowId;
-    rowId++;
+
 
     for(var j = 0; j < 8; j++){
       if(currentColor >= colors.length){
@@ -17,13 +33,34 @@
       }
       var square = document.createElement('div');
       square.className = 'square';
-      square.id = 'square' + squareId;
+      square.id = rowId + '' + squareId;
       square.style.backgroundColor = colors[currentColor];
       squareId++;
       row.appendChild(square);
       currentColor++;
+      if(squareId > 7){
+        squareId = 0;
+      }
     }
+    rowId++;
     board.appendChild(row);
-
   }
-})();
+}
+
+function setPieces(){
+  for(var i = 0; i < boardSquares.length; i++){
+    for(var j = 0; j < boardSquares[i].length; j++){
+      if(boardSquares[i][j] === 'red'){
+        var img = document.createElement('img');
+        img.src = 'images/red.png';
+        img.className = 'checker-piece';
+        document.getElementById(i + '' + j).appendChild(img);
+      }else if(boardSquares[i][j] === 'black'){
+        var img = document.createElement('img');
+        img.src = 'images/black.png';
+        img.className = 'checker-piece';
+        document.getElementById(i + '' + j).appendChild(img);
+      }
+    }
+  }
+}
